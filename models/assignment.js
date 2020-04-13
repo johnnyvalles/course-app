fb1e2b375d9f057117e124047bec40ef4112b76f
Johnny Valles <vallesja@uci.edu>
2020-04-12 22:50:18 -0700

    Add course reference

--- models/assignment.js
+++ models/assignment.js
@@ -4,7 +4,8 @@ const assignmentSchema = new mongoose.Schema({
     name: { type: String, required: true },
     points: { type: Number, default: 100 },
     released: { type: Date, default: Date.now() },
-    due: { type: Date, default: Date.now() + 7 }
+    due: { type: Date, default: Date.now() + 7 },
+    course: { type: mongoose.Schema.Types.ObjectId, ref: "Course" }
 });
 
 module.exports = mongoose.model("Assignment", assignmentSchema);
