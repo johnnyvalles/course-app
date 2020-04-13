a62413e35279832e1a0fa990b18dcdd86033497e
Johnny Valles <vallesja@uci.edu>
2020-04-12 22:48:33 -0700

    Add student array references

--- models/course.js
+++ models/course.js
@@ -5,7 +5,11 @@ const courseSchema = new mongoose.Schema({
     instructor: { type: String, default: "TBA" },
     subject: { type: String, required: true },
     icon: { type: String, defualt: "bug" },
-    description: { type: String, required: true }
+    description: { type: String, required: true },
+    students: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Student"
+    }] 
 });
 
 module.exports = mongoose.model("Course", courseSchema);
