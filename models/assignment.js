922495a63caa167cf937e4692b4dbf26a92bc3fb
Johnny Valles <vallesja@uci.edu>
2020-04-12 22:50:57 -0700

    Make reference required

--- models/assignment.js
+++ models/assignment.js
@@ -5,7 +5,7 @@ const assignmentSchema = new mongoose.Schema({
     points: { type: Number, default: 100 },
     released: { type: Date, default: Date.now() },
     due: { type: Date, default: Date.now() + 7 },
-    course: { type: mongoose.Schema.Types.ObjectId, ref: "Course" }
+    course: { type: mongoose.Schema.Types.ObjectId, ref: "Course", required: true }
 });
 
 module.exports = mongoose.model("Assignment", assignmentSchema);
